890a593070973042cbbb8f0b57783f1062612640
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-06-13 03:08:06 +0530

    Updated

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pythcalculus',         
   packages = ['pythcalculus'],   
-  version = '0.1',      
+  version = '0.2',      
   license='GPL 2.0',        
   description = 'Integrate and Differentiate over here!',   
   author = 'Vaishakh Nargund',                  
   author_email = 'vaishakh.nargund1999@gmail.com',      
   url = 'https://github.com/vaish1999/Calculus-in-python',  
-  download_url = 'https://github.com/vaish1999/Calculus-in-python/archive/v1.0.tar.gz',   
+  download_url = 'https://github.com/vaish1999/Calculus-in-python/archive/v1.1.tar.gz',   
   keywords = ['calculus', 'python', 'differentiate','integrate','derivative'],   
   install_requires=[           
           'math',
