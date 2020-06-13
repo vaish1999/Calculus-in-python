4a867ce8752f467feb3bcb4b5c8351c6f12c6e19
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-06-13 13:54:51 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from os import path
-this_directory = path.abspath(path.dirname(_file_))
+this_directory = path.abspath(path.dirname("__file__"))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
